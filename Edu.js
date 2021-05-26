da6cebc366abdeeb6584fdc1670a11cfed70b710
DavidToja91 <hernan.rocha.toja@hotmail.com>
2021-05-26 11:01:22 -0300

    objeto literal integrantes

--- Edu.js
+++ Edu.js
@@ -1,2 +1,7 @@
 let word = "asdasd";
-let saludar = "Hola soy Edu y me olvide de saludar!"
+let saludar = "Hola! soy Edu y me olvide de saludar!"
+let integrantes ={
+    nombre : 'Devora',
+    nombre : 'Eduardo',
+    nombre: 'David'
+}
